9c223be7c0d20284ff4e7deae7fa50ecd5817d48
wendesongomes <wendeson.gomes@live.com>
2025-01-03 14:35:47 -0300

    docs: adjust version and remove dev dependecies

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import dts from "vite-plugin-dts";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
   plugins: [
@@ -11,7 +10,6 @@ export default defineConfig({
       insertTypesEntry: true,
       outDir: "dist/types",
     }),
-    cssInjectedByJsPlugin(),
   ],
   build: {
     lib: {
